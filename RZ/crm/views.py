ad6ddfef59ee6bb0d4273f636f5dea44c5de4aad
Breakering <1079614505@qq.com>
2017-08-24 17:54:45 +0800

    update views.py

--- RZ/crm/views.py
+++ RZ/crm/views.py
@@ -155,24 +155,28 @@ class Daily(View):
         return result
 
     def get_info(self, qdate):
-        base_info = models.BaseInfo.objects.using("default").filter(qdate=qdate).first()
-        tg_info = models.TgInfo.objects.using("default").filter(qdate=qdate).first()
-        operate_info = models.OperateInfo.objects.using("default").filter(qdate=qdate).first()
-        invite_info = models.InviteInfo.objects.using("default").filter(qdate=qdate).first()
-        asset_info = models.AssetInfo.objects.using("default").filter(qdate=qdate).first()
-        kefu_info = models.KeFuInfo.objects.using("default").filter(qdate=qdate).first()
-        # 存放第一页数据
-        daily_page1 = {}
-        daily_page1["tz_zh"] = round(int(base_info.xztz_r) / int(base_info.zhu_r) * 100, 2)  # 当日投资转化率
-        daily_page1["tz_j"] = round(int(base_info.tz_j) / 10000, 2)  # 投资金额
-        daily_page1["xztz_j"] = round(int(base_info.xztz_j) / 10000, 2)  # 新增投资金额
-        daily_page1["xztz_j_zb"] = round(int(base_info.xztz_j) / int(base_info.tz_j) * 100, 0)  # 新增投资金额占比
-        daily_page1["sm_r"] = int(base_info.sm_r)  # 实名人数
-        daily_page1["zhu_r"] = int(base_info.zhu_r)  # 注册人数
-        daily_page1["tz_r"] = int(base_info.tz_r)  # 投资人数
-        daily_page1["xztz_r"] = int(base_info.xztz_r)  # 新增投资人数
-        daily_page1["pg_tz_j"] = round(int(base_info.tz_j) / int(base_info.tz_r) / 10000, 1)  # 平均每人投资
-        daily_page1["tj_r"] = int(invite_info.invited_st_r)
+        try:
+            base_info = models.BaseInfo.objects.using("default").filter(qdate=qdate).first()
+            tg_info = models.TgInfo.objects.using("default").filter(qdate=qdate).first()
+            operate_info = models.OperateInfo.objects.using("default").filter(qdate=qdate).first()
+            invite_info = models.InviteInfo.objects.using("default").filter(qdate=qdate).first()
+            asset_info = models.AssetInfo.objects.using("default").filter(qdate=qdate).first()
+            kefu_info = models.KeFuInfo.objects.using("default").filter(qdate=qdate).first()
+            # 存放第一页数据
+            daily_page1 = {}
+            daily_page1["qdate"] = qdate  # 当前日期
+            daily_page1["tz_zh"] = round(int(base_info.xztz_r) / int(base_info.zhu_r) * 100, 2)  # 当日投资转化率
+            daily_page1["tz_j"] = round(int(base_info.tz_j) / 10000, 2)  # 投资金额
+            daily_page1["xztz_j"] = round(int(base_info.xztz_j) / 10000, 2)  # 新增投资金额
+            daily_page1["xztz_j_zb"] = round(int(base_info.xztz_j) / int(base_info.tz_j) * 100, 0)  # 新增投资金额占比
+            daily_page1["sm_r"] = int(base_info.sm_r)  # 实名人数
+            daily_page1["zhu_r"] = int(base_info.zhu_r)  # 注册人数
+            daily_page1["tz_r"] = int(base_info.tz_r)  # 投资人数
+            daily_page1["xztz_r"] = int(base_info.xztz_r)  # 新增投资人数
+            daily_page1["pg_tz_j"] = round(int(base_info.tz_j) / int(base_info.tz_r) / 10000, 1)  # 平均每人投资
+            daily_page1["tj_r"] = int(invite_info.invited_st_r)
+        except Exception as e:
+            daily_page1 = None
         return daily_page1
 
     def get(self, request, *args, **kwargs):
