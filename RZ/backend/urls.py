01d7b9bbddfc220d15393fd481b32815341493ed
Breakering <1079614505@qq.com>
2017-09-15 14:18:39 +0800

    start a new app backend

--- RZ/backend/urls.py
+++ RZ/backend/urls.py
@@ -14,11 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.conf.urls import include
-from django.contrib import admin
+from .views import user
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^crm/', include("crm.urls")),
-    url(r'^backend/', include("backend.urls")),
+    url(r'^login/', user.login),
 ]
---
+++ RZ/backend/views/__init__.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# __author__ = "Breakering"
+# Date: 2017/9/15
+
