c2b2ca556baefb26c88d6085558cd929648a9977
Breakering <1079614505@qq.com>
2017-09-21 09:45:03 +0800

    update rzjf_asset_info.sql

--- RZ/crm/RzSql/daily/rzjf_asset_info.sql
+++ RZ/crm/RzSql/daily/rzjf_asset_info.sql
@@ -3,10 +3,10 @@ from
 (
 SELECT qdate,case when term < 10 then "A:1-10天"
 when term >= 10 and term < 30 then "B:10-30天"
-when term >= 30 and term < 60 then "C:30-60天"
-when term >= 60 and term < 90 then "D:60-90天"
-when term >= 90 and term < 180 then "E:90-180天"
-else "F:180天及以上"
+when term >= 30 and term < 60 then "C:1月标"
+when term >= 60 and term < 90 then "D:2月标"
+when term >= 90 and term < 180 then "E:3月标"
+else "F:6月标及以上"
 end term,tz_r,tz_j,mb_ys
 from rzjf_asset_info
 where unix_timestamp(qdate) >= unix_timestamp("{qdate}") - {section} * 86400 + 86400
