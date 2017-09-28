e5565fe132521d688624d936c6ab8633fe90c073
Breakering <1079614505@qq.com>
2017-09-28 15:04:28 +0800

    添加手机号中间四位和姓名后面几位隐藏功能

--- RZ/crm/templatetags/mytag.py
+++ RZ/crm/templatetags/mytag.py
@@ -11,3 +11,15 @@ register = template.Library()  # 对象名必须为register
 @register.simple_tag
 def DailyDate(qdate):
     return datetime.strftime(qdate, "%Y年%m月%d")
+
+
+@register.filter
+def phone_filter(phone):
+    """手机号中间四位隐藏"""
+    return "%s%s%s" % (phone[0:3], "****", phone[7:])
+
+
+@register.filter
+def name_filter(name):
+    """姓名后面几位隐藏"""
+    return "%s%s" % (name[0:1], "*" * len(name[1:]))
