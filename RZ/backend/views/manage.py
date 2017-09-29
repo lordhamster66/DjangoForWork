4df3a57b84a3bb0cc035a9e051b43c6ca14573bc
Breakering <1079614505@qq.com>
2017-09-29 16:10:02 +0800

    修复网贷天眼分页HTML显示个数问题

--- RZ/backend/views/manage.py
+++ RZ/backend/views/manage.py
@@ -158,7 +158,7 @@ def wdzj_jk(request):
     )
     response = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64)"})
     data = response.json() if response.status_code == 200 else {"totalCount": 0, "borrowList": []}  # 获取接口返回的数据
-    data_count = int(data.get("totalCount", 0))  # 获取总页数
+    data_count = int(data.get("totalCount", 0))  # 获取总个数
     info_list = data.get("borrowList")  # 获取当前页内容
     page_obj = Page(current_page, data_count)  # 生成分页对象
     page_str = page_obj.page_str("/backend/wdzj_jk/")  # 获取分页html
@@ -199,7 +199,7 @@ def wdty_jk(request):
     )
     response = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64)"})
     data = response.json() if response.status_code == 200 else {"page_count": 0, "loans": []}  # 获取接口返回的数据
-    data_count = int(data.get("page_count", 0))  # 获取总页数
+    data_count = int(data.get("page_count", 0)) * num_show  # 获取总个数
     info_list = data.get("loans")  # 获取当前页内容
     page_obj = Page(current_page, data_count)  # 生成分页对象
     page_str = page_obj.page_str("/backend/wdty_jk/")  # 获取分页html
