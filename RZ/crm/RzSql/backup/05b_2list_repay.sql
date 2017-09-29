2077db2ef917cd0d50d89379796951af20f5e5ac
Breakering <1079614505@qq.com>
2017-09-29 17:11:53 +0800

    技术现无需这些表的支持，因此删除rzjf_bi库表01u_2borrower、01u_2tender、05b_2list_repy、borrow_tender、zzz_ctlog_c_st01、zzz_ctlog_c_uid、zzz_ctlog_day_st01

--- RZ/crm/RzSql/backup/05b_2list_repay.sql
+++
@@ -1,2 +0,0 @@
-insert into 05b_2list_repay select * from wd.05b_2list_repay
-where time_h > DATE_FORMAT(DATE_SUB(curdate(),INTERVAL 1 DAY),"%Y-%m-%d 23:30:00") and time_h <= DATE_FORMAT(curdate(),"%Y-%m-%d 23:30:00");
