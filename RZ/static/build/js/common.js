fa2a35be91836d3e20c7188190aa1259a7e64d98
Breakering <1079614505@qq.com>
2017-09-30 17:04:52 +0800

    日报头部增加导航,可以查看跳转至月度数据，目前月度数据还没开发

--- RZ/static/build/js/common.js
+++ RZ/static/build/js/common.js
@@ -8,3 +8,8 @@ $(function () {
         dateFormat: "yy-mm-dd"
     });
 });
+$(".subNav").click(function () {
+    $(this).toggleClass("currentDd").siblings(".subNav").removeClass("currentDd");
+    $(this).toggleClass("currentDt").siblings(".subNav").removeClass("currentDt");
+    $(this).next(".navContent").slideToggle(300).siblings(".navContent").slideUp(500)
+})
