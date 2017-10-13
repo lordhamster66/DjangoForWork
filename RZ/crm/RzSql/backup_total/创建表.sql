ca4de2b3879a911e6599f9278b049e81c7bbd78d
Breakering <1079614505@qq.com>
2017-10-13 15:14:44 +0800

    rzjf_bi新增表05b_2list_recover

--- RZ/crm/RzSql/backup_total/创建表.sql
+++ RZ/crm/RzSql/backup_total/创建表.sql
@@ -12,6 +12,9 @@ insert into 01u_0base select * from wd.01u_0base WHERE time_h <= DATE_FORMAT(DAT
 CREATE TABLE 05b_7dsbid like wd.05b_7dsbid;
 insert into 05b_7dsbid select * from wd.05b_7dsbid WHERE time_h <= DATE_FORMAT(DATE_SUB(curdate(),INTERVAL 1 DAY),"%Y-%m-%d 23:30:00");
 
+CREATE TABLE 05b_2list_recover like wd.05b_2list_recover;
+insert into 05b_2list_recover select * from wd.05b_2list_recover WHERE time_h <= DATE_FORMAT(DATE_SUB(curdate(),INTERVAL 1 DAY),"%Y-%m-%d 23:30:00");
+
 create table rz_borrow like new_wd.rz_borrow;
 insert into rz_borrow select * from new_wd.rz_borrow WHERE create_time <= DATE_FORMAT(DATE_SUB(curdate(),INTERVAL 1 DAY),"%Y-%m-%d 23:30:00");
 
