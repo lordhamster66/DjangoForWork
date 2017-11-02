02941c2fec3a0cd58ff1bcdc8c0b936164c5005c
Breakering <1079614505@qq.com>
2017-11-02 10:37:18 +0800

    各端回款并提现中回款去掉R计划自动续投部分

--- RZ/crm/RzSql/daily/geduan_rw.sql
+++ RZ/crm/RzSql/daily/geduan_rw.sql
@@ -56,40 +56,9 @@ from
 																		and r.real_total > 0
 																		UNION ALL
 																		SELECT a3.user_id uid,a3.repayment_amount recover_account,a3.repayment_time recover_times
-																		from (
-																					SELECT * from new_wd.rz_borrow_collection_0 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_1 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_2 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_3 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_4 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_5 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_6 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_7 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_8 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_9 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_10 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_11 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_12 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_13 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_14 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_15 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_16 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_17 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_18 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_19 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_20 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_21 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_22 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_23 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_24 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_25 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_26 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_27 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_28 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_29 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_30 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY) UNION ALL
-																					SELECT * from new_wd.rz_borrow_collection_31 where status in (0,1) and DATE(repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY)
-																				) a3
+																		from new_wd.rz_borrow_collection a3
+																		where a3.status in (0,1)
+																		and DATE(a3.repayment_time) = DATE_SUB(curdate(),INTERVAL 1 DAY)
 												       )  a
 														LEFT JOIN 01u_0base b on a.uid=b.uid
 														LEFT JOIN 01u_0info c on a.uid=c.uid
