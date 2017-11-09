27753aeef109236fa5009463fda32fe7ad18646b
Breakering <1079614505@qq.com>
2017-11-09 11:30:56 +0800

    邀请SQL代码更新(佣金部分进行了修改)

--- RZ/crm/RzSql/daily/invite_info.sql
+++ RZ/crm/RzSql/daily/invite_info.sql
@@ -144,18 +144,57 @@ INNER JOIN
 ) t5 on a.nid = t5.nid
 INNER JOIN
 (
-		SELECT 1 nid,sum(a.money) '佣金(新版)'
-		from rz_invite_flow a
-		where a.brokerage <> 0
-		and a.`status` = 1
-		and a.ctime >= DATE_SUB(CURDATE(),INTERVAL 1 day)
-		and a.ctime < CURDATE()
-		and a.buid in
+		SELECT 1 nid,(sum(a.`利息`)*0.1) '佣金(新版)'
+		FROM
 		(
-		SELECT a.buid uid
-		from  rz_invite_user a
-		where a.reg_time < CURDATE()
-		)
+				SELECT sum(a.利息)利息
+				FROM
+				(
+						SELECT a.uid,ROUND((a.apr/100*a.qixian/365*a.account),2)利息
+						from
+						(
+						SELECT a3.borrow_id bid,t3.name,t3.apr,'0' aprplus,
+						case when t3.borrow_time_type!=0 then t3.time_limit else t3.time_limit*30 end qixian,
+						case when t3.borrow_time_type!=0 then a3.real_amount*t3.time_limit/360 else a3.real_amount*t3.time_limit/12 end nianhua,
+						a3.user_id uid,a3.real_amount account,a3.create_time time_h,'0' hbid
+						from new_wd.rz_borrow_tender a3
+						INNER JOIN new_wd.rz_borrow_big t3 on a3.borrow_id = t3.id
+						where a3.`status` = 1 and t3.product_id = 10020
+						and a3.create_time >= DATE_SUB(CURDATE(),INTERVAL 1 day)
+						and a3.create_time < CURDATE()
+						) a
+						union ALL
+						SELECT b.uid,b.recover_interest
+						FROM
+						(
+						SELECT a1.uid,a1.time_h,t1.name,
+						case when t1.days!=0 then t1.days else t1.borrow_period*30 end qixian,
+						a1.recover_account,a1.recover_capital,a1.recover_interest,
+						a1.recover_times,a1.bid,t1.borrow_apr
+						from 05b_2list_recover a1
+						INNER JOIN 05b_0base t1 on a1.bid = t1.bid
+						where a1.bid <> 10000 and a1.recover_status in (0,1)
+						and a1.time_h >= DATE_SUB(CURDATE(),INTERVAL 1 day)
+						and a1.time_h < CURDATE()
+						UNION ALL
+						SELECT a3.user_id uid,a3.create_time time_h,t1.name,
+						case when t1.borrow_time_type!=0 then t1.time_limit else t1.time_limit*30 end qixian,
+						a3.repayment_amount recover_account,a3.capital recover_capital,a3.interest recover_interest,
+						a3.repayment_time recover_times,a3.borrow_id,t1.apr
+						from new_wd.rz_borrow_collection a3
+						INNER JOIN new_wd.rz_borrow_big t1 on a3.borrow_id = t1.id
+						where a3.`status` in (0,1) and t1.product_id <> 10020
+						and a3.create_time >= DATE_SUB(CURDATE(),INTERVAL 1 day)
+						and a3.create_time < CURDATE()
+						) b
+				) a
+				INNER JOIN
+				(
+						SELECT DISTINCT a.buid uid
+						from  rz_invite_user a
+						where a.reg_time < CURDATE()
+				) z on a.uid = z.uid
+		) a
 ) t6 on a.nid = t6.nid
 INNER JOIN
 (
@@ -371,28 +410,66 @@ INNER JOIN
 				) b on a.buid = b.buid and a.ctime = b.ctime
 		union all
 				SELECT 0,0,0,sum(a.money)佣金
-				FROM
-				(
-						SELECT a.*
-						from rz_invite_flow a
-						where a.`status` = 1 and a.brokerage <> 0
-						and a.ctime >= DATE_SUB(CURDATE(),INTERVAL 1 day)
-						and a.ctime < CURDATE()
-						and a.buid in
-						(
-						SELECT a.buid uid
-						from  rz_invite_user a
-						where a.reg_time < CURDATE()
-						)
-				) a
-				INNER JOIN
-				(
-						SELECT a.buid,min(a.htid)htid
-						from rz_invite_flow a
-						where a.`status` = 1 and a.brokerage <> 0
-						and a.ctime >= DATE_SUB(CURDATE(),INTERVAL 1 day)
-						and a.ctime < CURDATE()
-						GROUP BY 1
-				) b on a.htid = b.htid and a.buid = b.buid
+					FROM
+					(
+								SELECT a.uid,a.account,a.borrow_apr,a.qixian,(ROUND((a.borrow_apr/100*a.qixian/365*a.account),2)*0.1)money
+								from
+								(
+												SELECT a1.bid,t1.name,t1.borrow_apr,a1.aprplus,
+												case when t1.days!=0 then t1.days else t1.borrow_period*30 end qixian,
+												case when t1.days!=0 then a1.account*t1.days/360 else a1.account*t1.borrow_period/12 end nianhua,
+												a1.uid,a1.account,a1.time_h,a1.hbid
+												from 05b_1tenderfinal a1
+												INNER JOIN 05b_0base t1 on a1.bid = t1.bid
+												where a1.orguid = 0 and a1.bid <> 10000 and a1.status in (1,3)
+												and a1.time_h >= DATE_SUB(CURDATE(),INTERVAL 1 day)
+												and a1.time_h < CURDATE()
+												union all
+												SELECT a2.borrow_id bid,t2.name,t2.apr,'0' aprplus,
+												case when t2.borrow_time_type!=0 then t2.time_limit else t2.time_limit*30 end qixian,
+												case when t2.borrow_time_type!=0 then a2.real_amount*t2.time_limit/360 else a2.real_amount*t2.time_limit/12 end nianhua,
+												a2.user_id uid,a2.real_amount account,a2.add_time time_h,'0' hbid
+												from new_wd.borrow_tender a2
+												INNER JOIN new_wd.borrow t2 on a2.borrow_id = t2.id
+												where a2.status = 1
+												and a2.add_time >= DATE_SUB(CURDATE(),INTERVAL 1 day)
+												and a2.add_time < CURDATE()
+												union all
+												SELECT a3.borrow_id bid,t3.name,t3.apr,'0' aprplus,
+												case when t3.borrow_time_type!=0 then t3.time_limit else t3.time_limit*30 end qixian,
+												case when t3.borrow_time_type!=0 then a3.real_amount*t3.time_limit/360 else a3.real_amount*t3.time_limit/12 end nianhua,
+												a3.user_id uid,a3.real_amount account,a3.create_time time_h,'0' hbid
+												from new_wd.rz_borrow_tender a3
+												INNER JOIN new_wd.rz_borrow_big t3 on a3.borrow_id = t3.id
+												where a3.`status` = 1
+												and a3.create_time >= DATE_SUB(CURDATE(),INTERVAL 1 day)
+												and a3.create_time < CURDATE()
+								) a
+								INNER JOIN
+								(
+												SELECT h1.uid,min(h1.time_h) min_time
+												from
+												(
+												SELECT a1.uid,a1.account,a1.time_h
+												from 05b_1tenderfinal a1
+												where a1.orguid = 0 and a1.status in (1,3) and a1.bid <> 10000
+												union all
+												SELECT a2.user_id uid,a2.real_amount account,a2.add_time time_h
+												from new_wd.borrow_tender a2
+												where a2.status = 1
+												union all
+												select a3.user_id uid,a3.real_amount account,a3.create_time time_h
+												from new_wd.rz_borrow_tender a3
+												where a3.status = 1
+												) h1
+												GROUP BY h1.uid
+								) t on a.uid = t.uid and a.time_h = t.min_time
+								INNER JOIN
+								(
+												SELECT DISTINCT a.buid uid
+												from  rz_invite_user a
+												where a.reg_time < CURDATE()
+								) z on a.uid = z.uid
+					) a
 		) a
 ) t12 on a.nid = t12.nid
