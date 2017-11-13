d08b49c8095579f817b647ee814a3719f4d6cda3
Breakering <1079614505@qq.com>
2017-11-13 15:59:56 +0800

    DataManagement项目数据库表结构创建成功

--- DataManagement/dm/admin.py
+++ DataManagement/dm/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from dm import models
 
 # Register your models here.
+admin.site.register(models.UserProfile)
+admin.site.register(models.Role)
+admin.site.register(models.Menu)
+admin.site.register(models.Department)
+admin.site.register(models.SQLRecord)
+admin.site.register(models.SQLContent)
+admin.site.register(models.SQLTag)
