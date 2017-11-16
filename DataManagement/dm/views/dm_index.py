4bb57fe02e855fa637b9c1fa408b1521d80d37ec
Breakering <1079614505@qq.com>
2017-11-16 08:49:42 +0800

    对dm首页的空值做了处理，如果出现空值则置为0

--- DataManagement/dm/views/dm_index.py
+++ DataManagement/dm/views/dm_index.py
@@ -26,21 +26,20 @@ def index(request):
     home_dict["sm_r"] = get_info_list("rz", get_sql_content(11))[0].get("sm_r")
     home_dict["tz_r"] = tz_info.get("tz_r")
     home_dict["amount"] = tz_info.get("amount")
-    home_dict["g_amount"] = get_info_list("rz", get_sql_content(21))[0].get("g_amount")
-    home_dict["un_r_auto_x_amount"] = get_info_list("rz", get_sql_content(20))[0].get("un_r_auto_x_amount")
-    home_dict["r_auto_amount"] = get_info_list("rz", get_sql_content(19))[0].get("r_auto_amount")
+    home_dict["g_amount"] = get_info_list("rz", get_sql_content(21))[0].get("g_amount") or 0
+    home_dict["un_r_auto_x_amount"] = get_info_list("rz", get_sql_content(20))[0].get("un_r_auto_x_amount") or 0
+    home_dict["r_auto_amount"] = get_info_list("rz", get_sql_content(19))[0].get("r_auto_amount") or 0
     home_dict["un_r_auto_recover_account_list"].append(
         float(get_info_list("rz", get_sql_content(22).format(date="DATE_SUB(CURDATE(),INTERVAL 1 day)"))[0].get(
-            "un_r_auto_recover_account"
-        ))  # todo 有可能无回款,几乎不可能出现
+            "un_r_auto_recover_account") or 0)
     )
     home_dict["un_r_auto_recover_account_list"].append(
-        float(get_info_list("rz", get_sql_content(22).format(date="CURDATE()"))[0].get("un_r_auto_recover_account"))
+        float(get_info_list("rz", get_sql_content(22).format(date="CURDATE()"))[0].get(
+            "un_r_auto_recover_account") or 0)
     )
     home_dict["un_r_auto_recover_account_list"].append(
         float(get_info_list("rz", get_sql_content(22).format(date="DATE_ADD(CURDATE(),INTERVAL 1 day)"))[0].get(
-            "un_r_auto_recover_account"
-        ))
+            "un_r_auto_recover_account") or 0)
     )
     return render(request, "index.html", {
         "home_dict": home_dict
