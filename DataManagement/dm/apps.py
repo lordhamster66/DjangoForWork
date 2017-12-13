0fb78221d16ea1df3b15301c366131ce5cd87eea
Breakering <1079614505@qq.com>
2017-12-13 09:55:35 +0800

    删除DataManagement项目,已经用RzAdmin项目取代

--- DataManagement/dm/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DmConfig(AppConfig):
-    name = 'dm'
--- DataManagement/dm/views/__init__.py
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-# __author__ = "Breakering"
-# Date: 2017/11/13
-
