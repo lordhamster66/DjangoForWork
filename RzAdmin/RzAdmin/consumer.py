6837d98030e6d81d56a4ad720d0d69ccf99f9e8f
Breakering <1079614505@qq.com>
2017-12-25 16:35:45 +0800

    RzAdmin添加实时消息展示功能，实现的方式较之前更加简单便捷且高效

--- RzAdmin/RzAdmin/consumer.py
+++ RzAdmin/RzAdmin/consumer.py
@@ -2,41 +2,15 @@
 # -*- coding: utf-8 -*-
 # __author__ = "Breakering"
 # Date: 2017/12/22
-import pika
 import json
-import threading
 from django.http import HttpResponse
 from channels.handler import AsgiHandler
+
+message_dict = {}  # 存储用户连上来的message
 
 
 # message.reply_channel    一个客户端通道的对象
 # message.reply_channel.send(chunk)  用来唯一返回这个客户端
-
-
-def receive(message):
-    email = message["text"]
-    username = 'breakering'
-    password = 'dmc19930417'
-    host = '10.1.1.105'
-    credentials = pika.PlainCredentials(username, password)
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host=host, credentials=credentials, port=5672)
-    )
-    channel = connection.channel()
-    channel.exchange_declare(exchange='direct_logs', exchange_type='direct')
-    result = channel.queue_declare(exclusive=True)
-    queue_name = result.method.queue
-    channel.queue_bind(exchange='direct_logs', queue=queue_name, routing_key=email)
-
-    def callback(ch, method, properties, body):
-        print(body)
-        message.reply_channel.send({"text": body.decode()})
-
-    channel.basic_consume(callback, queue=queue_name, no_ack=True)
-
-    channel.start_consuming()
-
-
 # 一个管道大概会持续30s
 
 # 当连接上时，发回去一个connect字符串
@@ -47,7 +21,7 @@ def ws_connect(message):
 # 将发来的信息原样返回
 def ws_message(message):
     """将发来的信息原样返回"""
-    pass
+    message_dict[message["text"]] = message
     # t = threading.Thread(target=receive, args=(message,))  # 用户连接服务器则启动一个线程用来监测rabbitmq
     # t.start()  # 启动线程
 
--- RzAdmin/automatic/automatic_signals.py
+++ RzAdmin/automatic/automatic_signals.py
@@ -4,6 +4,7 @@
 # Date: 2017/12/13
 import pika
 import json
+from RzAdmin import settings
 from django.core.signals import request_finished  # 请求结束后
 from django.core.signals import request_started  # 请求到来前
 from django.core.signals import got_request_exception  # 请求异常后
@@ -20,34 +21,39 @@ from django.test.signals import template_rendered  # 使用test测试渲染模�
 
 from django.db.backends.signals import connection_created  # 创建数据库连接时
 
-
-# username = 'breakering'
-# password = 'dmc19930417'
-# host = '10.1.1.105'
-# credentials = pika.PlainCredentials(username, password)
-# connection = pika.BlockingConnection(
-#     pika.ConnectionParameters(host=host, credentials=credentials, port=5672)
-# )
-# channel = connection.channel()
+from RzAdmin.consumer import message_dict
 
 
 def model_instance_save_callback(sender, **kwargs):
     """model对象保存时的回调函数"""
-    pass
-    # if sender._meta.model_name == "downloadrecord":  # 说明有人在更新下载记录，即审核
-    #     download_record_obj = kwargs.get("instance")  # 用户下载记录对象
-    #     channel.exchange_declare(exchange='direct_logs', exchange_type="direct")
-    #     severity = download_record_obj.user.email  # 下载用户邮箱，唯一标识
-    #     message = json.dumps({
-    #         "title": download_record_obj.download_detail,
-    #         "message": "已有更新，请去用户中心查看!", "alert_type": "success"
-    #     })
-    #     channel.basic_publish(
-    #         exchange='direct_logs',
-    #         routing_key=download_record_obj.user.email,
-    #         body=message
-    #     )
-    #     connection.close()
+    from automatic import models
+    if sender._meta.model_name == "downloadrecord":  # 说明有人在更新下载记录，即审核
+        download_record_obj = kwargs.get("instance")  # 用户下载记录对象
+        if kwargs.get("created"):  # 说明用户在创建下载记录
+            mass_users = []  # 要群发的用户邮箱
+            detaile_jurisdiction_role_objs = models.Role.objects.filter(name__in=settings.DetaileJurisdiction).all()
+            for detaile_jurisdiction_role_obj in detaile_jurisdiction_role_objs:
+                for user_obj in detaile_jurisdiction_role_obj.userprofile_set.all():
+                    user_email = user_obj.email
+                    if user_email in message_dict and user_email not in mass_users:
+                        mass_users.append(user_obj.email)
+            for user_email in mass_users:
+                message = message_dict.get(user_email)
+                if message:
+                    message.reply_channel.send({
+                        "text": json.dumps({
+                            "title": download_record_obj.download_detail,
+                            "message": "用户:%s,提交了新的下载审核，快去检查!" % download_record_obj.user.name,
+                            "alert_type": "info"
+                        })})
+        else:
+            message = message_dict.get(download_record_obj.user.email)  # 获取用户的websocket链接
+            if message:
+                message.reply_channel.send({
+                    "text": json.dumps({
+                        "title": download_record_obj.download_detail,
+                        "message": "已有更新，请去用户中心查看!", "alert_type": "success"
+                    })})
 
 
 post_save.connect(model_instance_save_callback)
