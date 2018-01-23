8133814cb0631e4b881501af07b4a8a0a0780f70
Breakering <1079614505@qq.com>
2018-01-23 09:46:24 +0800

    用户渠道标识SQL修正

--- RZ/crm/RzSql/storage/rzjf_user_memo.sql
+++ RZ/crm/RzSql/storage/rzjf_user_memo.sql
@@ -6,7 +6,7 @@ when a.regist_source_type = 1 then "WAP 官网"
 when a.regist_source_type in (2,3) then "APP 官网"
 else "PC 官网" end
 ,q.name
-)
+) memo
 from rz_user.rz_user_base_info a
 LEFT JOIN (SELECT `code`,name from rz_article.rz_channel GROUP BY `code`) q on a.utm_source = q.`code`
 where a.user_id not in
