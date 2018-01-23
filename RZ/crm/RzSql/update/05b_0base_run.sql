33e13e0908cef6da6f1ed2f253e27dfa94c29597
Breakering <1079614505@qq.com>
2018-01-23 17:57:22 +0800

    第三方接口更新数据SQL修正

--- RZ/crm/RzSql/update/05b_0base_run.sql
+++ RZ/crm/RzSql/update/05b_0base_run.sql
@@ -1,6 +1,6 @@
 UPDATE rzjf_bi.05b_0base_run a,rz_borrow.rz_borrow b
-set a.borrow_account_yes=b.amount_yes,
-a.borrow_account_wait=b.amount-b.amount_yes,
+set a.borrow_account_yes=b.amount,
+a.borrow_account_wait=b.amount-b.amount,
 a.borrow_account_scale=if(b.full_time="0000-00-00 00:00:00",'0','100')
 where a.bid=b.id
 and a.time_h > DATE_FORMAT(DATE_SUB(curdate(),INTERVAL 30 DAY),"%Y-%m-%d 23:30:00")
