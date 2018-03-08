47ee2c1d5f3c0e0d3d8ee7382afbbbcd3b82ec2a
Breakering <1079614505@qq.com>
2018-03-08 14:30:35 +0800

    停用之前存储业务数据项目的大部分功能,因为这些功能都已经转移至自动化后台项目

--- RZ/crm/urls.py
+++ RZ/crm/urls.py
@@ -24,20 +24,20 @@ from .views import exhibition
 
 urlpatterns = [
     url(r'^ceshi/$', ceshi.ceshi),  # 测试
-    url(r'^data_storage/$', storage.DataStorage.as_view()),  # 获取公司数据库信息
-    url(r'^daily/$', daily.Daily.as_view()),  # 日报
-    url(r'^monthly/$', daily.monthly),  # 月度数据
-    url(r'^get_wdzj_info/$', storage.get_wdzj_info),  # 爬取网贷之家数据信息
-    url(r'^get_wdty_info/$', storage.get_wdty_info),  # 爬取网贷天眼数据信息
-    url(r'^backup/$', db_backup.backup),  # 备份表
+    # url(r'^data_storage/$', storage.DataStorage.as_view()),  # 获取公司数据库信息
+    # url(r'^daily/$', daily.Daily.as_view()),  # 日报
+    # url(r'^monthly/$', daily.monthly),  # 月度数据
+    # url(r'^get_wdzj_info/$', storage.get_wdzj_info),  # 爬取网贷之家数据信息
+    # url(r'^get_wdty_info/$', storage.get_wdty_info),  # 爬取网贷天眼数据信息
+    # url(r'^backup/$', db_backup.backup),  # 备份表
     url(r'^login/$', views.login),  # 登录功能
     url(r'^register/$', views.register),  # 注册功能
     url(r'^logout/$', views.logout),  # 注销功能
-    url(r'^index/$', views.index),  # 首页
-    url(r'^wdzj/$', views.wdzj),  # 网贷之家
-    url(r'^create_fake_info/(?P<bid>\d*)/(?P<new_bid>\d*)/(?P<name>\w*-\w*)/(?P<full_date>\w*-\w*-\w*)/$',
-        some_script.create_fake_info),  # 创建虚假数据
-    url(r'^rzjf_recorde/$', storage.rzjf_recorde),  # 用于定时存储人众金服业务所产生的一些记录
-    url(r'^sales_achievement/$', exhibition.sales_achievement),  # 展示电销业绩
-    url(r'^rzjf_invest_rank/$', storage.rzjf_invest_rank),  # 存储投资次数
+    # url(r'^index/$', views.index),  # 首页
+    # url(r'^wdzj/$', views.wdzj),  # 网贷之家
+    # url(r'^create_fake_info/(?P<bid>\d*)/(?P<new_bid>\d*)/(?P<name>\w*-\w*)/(?P<full_date>\w*-\w*-\w*)/$',
+    #    some_script.create_fake_info),  # 创建虚假数据
+    # url(r'^rzjf_recorde/$', storage.rzjf_recorde),  # 用于定时存储人众金服业务所产生的一些记录
+    # url(r'^sales_achievement/$', exhibition.sales_achievement),  # 展示电销业绩
+    # url(r'^rzjf_invest_rank/$', storage.rzjf_invest_rank),  # 存储投资次数
 ]
